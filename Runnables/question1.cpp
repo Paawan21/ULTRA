a138a41d993813a8d143a616d853a34d789c5a6f
Paawan kashyap <120301133+Paawan21@users.noreply.github.com>
2024-03-26 17:22:42 +0530

    Add files via upload

--- Runnables/question1.cpp
+++ Runnables/question1.cpp
@@ -1,165 +1,70 @@
-#include "/Users/paawankashyap/Desktop/ULTRA/ULTRA/DataStructures/Graph/Classes/StaticGraph.h"
-#include "/Users/paawankashyap/Desktop/ULTRA/ULTRA/DataStructures/Graph/Utils/Utils.h"
-#include "/Users/paawankashyap/Desktop/ULTRA/ULTRA/Algorithms/Dijkstra/Dijkstra.h"
-#include "/Users/paawankashyap/Desktop/ULTRA/ULTRA/DataStructures/Graph/Graph.h"
-#include "/Users/paawankashyap/Desktop/ULTRA/ULTRA/DataStructures/Graph/Utils/IO.h"
-#include "/Users/paawankashyap/Desktop/ULTRA/ULTRA/DataStructures/Graph/Classes/GraphInterface.h"
+
+#include "../Algorithms/Dijkstra/Dijkstra.h"
+#include "../DataStructures/Graph/Classes/StaticGraph.h" 
+#include "../DataStructures/Graph/Graph.h"
+#include "../DataStructures/Graph/Utils/IO.h"
+#include "../Helpers/Timer.h"
 #include <iostream>
-#include <vector>
-#include <random>
-#include <chrono>
-#include <string>
-#include <numeric> 
-using namespace std;
-using namespace Graph;
+#include <map>
 
-int main() {
-    StaticGraph<
-        Meta::List<Attribute<BeginOut, Edge>, Attribute<OutDegree, size_t>, Attribute<IncomingEdges, std::vector<Edge>>, Attribute<Coordinates, Geometry::Point>, Attribute<Weight, int>>, 
-        Meta::List<Attribute<Valid, bool>, Attribute<IncomingEdgePointer, size_t>, Attribute<FromVertex, Vertex>, Attribute<ToVertex, Vertex>>
-    > graph;
-    
-    // Load graph from DIMACS file
-    fromDimacs("/Users/paawankashyap/Downloads/USA-road-d.FLA/USA-road-d.FLA", graph);
-    vector<Vertex> validVertices;
-    for (Vertex v : graph.vertices()) {
-        if(v.isValid() == true) {
-            validVertices.push_back(v);
-        }
-    }
-    if (validVertices.empty()) {
-        cout << "No valid vertices found." << endl;
-        return 0;
-    }
+int main(){
+    StaticGraph<NoVertexAttributes,WithTravelTime> graph;
+    Graph::fromDimacs("./USA-road-d.FLA.gr",graph);
+    Dijkstra<StaticGraph<NoVertexAttributes,WithTravelTime>> dijkstra(graph);
+   
 
-    random_device rd;
-    mt19937 gen(rd());  // Standard mersenne_twister_engine seeded with rd() 
-    uniform_int_distribution<> dis(0, validVertices.size() - 1);
+    Range<Vertex> graph_vertices = graph.vertices();
+
+    std::random_device random_gen; //non-deterministic distributor
+    std::mt19937 gen(random_gen()); //Mersene twister
+
     
-    // Print basic information about the graph
-    cout << "Graph shape is " << graph.numVertices() << " vertices and " << graph.numEdges() << " edges." << endl;
-    auto start = chrono::high_resolution_clock::now();
-    // Define source and target vertices
-    Vertex src = validVertices[dis(gen)-1];  
-    Vertex trgt = validVertices[dis(gen)-1]; 
+    int number_of_vertices = graph_vertices.size();
+
+    Timer timer;
     
-    // Check if the source and target vertices are valid
-    if (!graph.isVertex(src) || !graph.isVertex(trgt)) {
-        cout << "Source and/or target vertices are not valid." << endl;
-        return 0;
+    for(size_t i = 0; i<200;++i){
+        std::uniform_int_distribution<> dis(0,number_of_vertices-i-1);
+        int random_source = dis(gen);
+        int random_destination = dis(gen);
+
+        Vertex random_source_vertex = graph_vertices[random_source];
+        Vertex random_destination_vertex = graph_vertices[random_destination];
+
+        dijkstra.run(random_source_vertex,random_destination_vertex);
+
+        std::vector<Vertex> path = dijkstra.getPath(random_destination_vertex);
+
+        int distance = dijkstra.getDistance(random_destination_vertex);
+
     }
 
-    // Create Dijkstra instance and run algorithm
-    Dijkstra<StaticGraph<
-        Meta::List<Attribute<BeginOut, Edge>, Attribute<OutDegree, size_t>, Attribute<IncomingEdges, std::vector<Edge>>, Attribute<Coordinates, Geometry::Point>, Attribute<Weight, int>>, 
-        Meta::List<Attribute<Valid, bool>, Attribute<IncomingEdgePointer, size_t>, Attribute<FromVertex, Vertex>, Attribute<ToVertex, Vertex>>
-    >, false> dijkstra(graph, graph[Weight]);
+    double milliseconds_passed = timer.elapsedMilliseconds();
+    std::cout<<"Total runtime in seconds for 200 random Dijkstra\'s : "<<milliseconds_passed/1000<<" seconds"<<std::endl;
 
-    // Run Dijkstra's algorithm
-    dijkstra.run(src, trgt);
 
-    // Retrieve shortest path and distance
-    int dist = dijkstra.getDistance(trgt);
-    vector<Vertex> sp = dijkstra.getPath(trgt);
-    
-    // Print distance from source to target
-    cout << "Distance from " << src << " to " << trgt << " is " << dist << endl;
 
-    // Print shortest path
-    if (!sp.empty()) {
-        cout << "Path from " << src << " to " << trgt << " is ";
-        for (Vertex v : sp) {
-            cout << v << " - ";
-        }
-        cout << endl;
+        
 
-        // Compute shortest path length
-        int shortestPathLength = 0;
-        for (int i = 0; i < sp.size() - 1; ++i) {
-            Vertex u = sp[i];
-            Vertex v = sp[i + 1];
-            bool edgeFound = false;
-            for (Edge e : graph.edgesFrom(u)) {
-                if (graph.get(ToVertex, e) == v) {
-                    shortestPathLength += graph[Weight][e];
-                    edgeFound = true;
-                    break;
-                }
-            }
-            if (!edgeFound) {
-                cout << "Error: Edge not found between vertices " << u << " and " << v << endl;
-                // Handle the error condition (optional)
-            }
-        }
+/*
+    //TEST CASES [VWorks]
+    //Source = 0, Target = 1
+    //Source = 264345, Target = 264327
+
+    Vertex source(1234);
+    Vertex target(1272);
+    dijkstra.run(source,target);
+
+    std::vector<Vertex> path = dijkstra.getPath(target);
+
+    for(Vertex vert : path){
+        std::cout<<vert<<", ";
     }
-    
-    auto stop = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::seconds>(stop - start);
-    cout << "Time taken by function: "
-        << duration.count() << " seconds" << endl;
+    std::cout<<std::endl;
+    int length = dijkstra.getDistance(target);
+    std::cout<<"Length: "<< length<<std::endl;
+
+*/
     return 0;
 }
 
-// #include "/Users/paawankashyap/Desktop/ULTRA/ULTRA/DataStructures/Graph/Classes/DynamicGraph.h"
-// #include "/Users/paawankashyap/Desktop/ULTRA/ULTRA/DataStructures/Graph/Utils/Utils.h"
-// #include "/Users/paawankashyap/Desktop/ULTRA/ULTRA/Algorithms/Dijkstra/Dijkstra.h"
-// #include "/Users/paawankashyap/Desktop/ULTRA/ULTRA/DataStructures/Graph/Graph.h"
-// #include "/Users/paawankashyap/Desktop/ULTRA/ULTRA/DataStructures/Graph/Utils/IO.h"
-// #include "/Users/paawankashyap/Desktop/ULTRA/ULTRA/DataStructures/Graph/Classes/GraphInterface.h"
-// #include <iostream>
-// #include <vector>
-// #include <random>
-// #include <chrono>
-// #include <string>
-// #include <numeric> // Include the <numeric> header for accumulate
-// using namespace std;
-// using namespace Graph;
-
-// int main() {
-//     DynamicGraphImplementation<
-//         Meta::InsertAttribute<Attribute<BeginOut, Edge>,
-//         Meta::InsertAttribute<Attribute<OutDegree, size_t>,
-//         Meta::InsertAttribute<Attribute<IncomingEdges, std::vector<Edge>>,
-//         Meta::SortAttributes<Meta::List<Attribute<Coordinates, Geometry::Point>, Attribute<Weight, int>>>>>>,
-//         Meta::InsertAttribute<Attribute<Valid, bool>,
-//         Meta::InsertAttribute<Attribute<IncomingEdgePointer, size_t>,
-//         Meta::InsertAttribute<Attribute<FromVertex, Vertex>,
-//         Meta::InsertAttribute<Attribute<ToVertex, Vertex>,
-//         Meta::SortAttributes<Meta::List<Attribute<Weight, int>>>>>>>> graph;
-    
-//     fromDimacs("/Users/paawankashyap/Downloads/USA-road-d.FLA/USA-road-d.FLA", graph);
-
-//     cout << "Graph shape is " << graph.numVertices() << " " << graph.numEdges() << endl;
-//     random_device rd;
-//     mt19937 gen(rd());
-//     uniform_int_distribution<> dis(0, graph.numVertices() - 1);
-    
-//     auto start = chrono::high_resolution_clock::now();
-//     for (int i = 0; i < 10; ++i) {
-//         Vertex src = Vertex(dis(gen));
-//         Vertex trgt = Vertex(dis(gen));
-
-//         Dijkstra<DynamicGraphImplementation<
-//             Meta::InsertAttribute<Attribute<BeginOut, Edge>,
-//             Meta::InsertAttribute<Attribute<OutDegree, size_t>,
-//             Meta::InsertAttribute<Attribute<IncomingEdges, std::vector<Edge>>,
-//             Meta::SortAttributes<Meta::List<Attribute<Coordinates, Geometry::Point>, Attribute<Weight, int>>>>>>,
-//             Meta::InsertAttribute<Attribute<Valid, bool>,
-//             Meta::InsertAttribute<Attribute<IncomingEdgePointer, size_t>,
-//             Meta::InsertAttribute<Attribute<FromVertex, Vertex>,
-//             Meta::InsertAttribute<Attribute<ToVertex, Vertex>,
-//             Meta::SortAttributes<Meta::List<Attribute<Weight, int>>>>>>>>, false> dijkstra(graph, graph[Weight]);
-//         dijkstra.run(src, trgt);
-//         vector<Vertex> sp = dijkstra.getPath(trgt);
-
-//         cout << "Path from " << src << " to " << trgt << " is ";
-//         for (Vertex v : sp) {
-//             cout << v << "-" << " ";
-//         }
-//         cout << endl;
-//     }
-//     auto end = chrono::high_resolution_clock::now();
-//     cout << "Time taken for 200 iterations is " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms" << endl;
-
-//     return 0;
-// }
